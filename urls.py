1c2364e2da9d4377a9f3292f109ecd72ed2b70e9
princessaerieldoromal <doromalprincessaeriel@gmail.com>
2026-03-12 09:26:34 +0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for firebase_dashboard project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path
 
 urlpatterns = [
-    path('add/', views.add_student_form, name='add_student'),  # Form page
-    path('', views.dashboard, name='dashboard'),               # Dashboard page
+    path('', include('dashboard.urls')),
 ]
